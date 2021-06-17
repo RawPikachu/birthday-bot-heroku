514242bc9e500579b11f38386d53b688bab10838
RawGamingYT <102355977z@gmail.com>
2021-06-16 20:19:47 -0400

    added bot owner checks

--- Music.py
+++ Music.py
@@ -16,7 +16,7 @@ class Music(commands.Cog):
         await ctx.send("An error occurred: {}".format(str(error)))
 
     @commands.command(name='join', invoke_without_subcommand=True, brief="The bot joins your voice channel.", description="This command makes the bot join your voice channel.")
-    @commands.has_role("DJ")
+    @commands.check_any(commands.is_owner(), commands.has_role("DJ"))
     async def _join(self, ctx):
         if ctx.author.voice:
             destination = ctx.author.voice.channel
@@ -30,7 +30,7 @@ class Music(commands.Cog):
             await ctx.send("You must be in a voice channel first.")
 
     @commands.command(name='leave', aliases=['disconnect', "stop"], brief="The bot leaves your voice channel.", description="This command makes the bot leave your voice channel.")
-    @commands.has_role("DJ")
+    @commands.check_any(commands.is_owner(), commands.has_role("DJ"))
     async def _leave(self, ctx):
         if ctx.voice_client:
             await ctx.guild.voice_client.disconnect()
@@ -39,7 +39,7 @@ class Music(commands.Cog):
             await ctx.send('Not connected to any voice channel.')
 
     @commands.command(name="play", brief="Joins your voice channel and plays the audio/video from the provided url.", description="This command makes the bot join your voice channel if you are not already in one and plays the audio/video from the provided url. (The url must end in an audio/video extension.)")
-    @commands.has_role("DJ")
+    @commands.check_any(commands.is_owner(), commands.has_role("DJ"))
     async def _play(self, ctx, url=None):
         if url == None:
             await ctx.send('You must provide a link providing an audio or video track.')
@@ -55,7 +55,7 @@ class Music(commands.Cog):
             await ctx.send("Playing audio track.")
 
     @commands.command(name="setservervolume", aliases=['setvolume', 'servervolume', 'volume'], brief="Sets the volume for the server.", description="This commands allows you to set the volume of audios being played in the server.")
-    @commands.has_permissions(administrator=True)
+    @commands.check_any(commands.is_owner(), commands.has_permissions(administrator=True))
     async def _setservervolume(self, ctx, volume=None):
         volume = float(volume)
         if volume == None or volume < 0 or volume > 100:
