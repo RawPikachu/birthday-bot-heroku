f99116ece4823e1a5b014b37e04044a4a0674c34
Chen <2519551@CSMV.QC.CA>
2021-06-18 10:35:29 -0400

    fixing shit

--- Music.py
+++ Music.py
@@ -85,6 +85,6 @@ class Music(commands.Cog):
     @commands.command(name="random", brief="*cough* Plays a random song.", description="*ahem* This commands allows you to play a random song in your voice channel.")
     @commands.check_any(commands.is_owner(), commands.has_role("DJ"))
     async def _random(self, ctx):
-        await ctx.invoke(self._play(ctx, url="https://cdn.discordapp.com/attachments/767132531350700062/855454319411068928/Never_Gonna_Give_You_Up_Original.mp3"))
+        await ctx.invoke(self._play, ctx, url="https://cdn.discordapp.com/attachments/767132531350700062/855454319411068928/Never_Gonna_Give_You_Up_Original.mp3")
         if ctx.voice_client:
             await ctx.send("haha jk.")
