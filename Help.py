c33b718929d0d9d40411f0afd27a440b273bf78b
RawGamingYT <56405082+RawGamingYT@users.noreply.github.com>
2021-09-29 19:35:49 -0400

    removed status page

--- Help.py
+++ Help.py
@@ -7,7 +7,7 @@ class MyHelpCommand(commands.MinimalHelpCommand):
         destination = self.get_destination()
         e = discord.Embed(title="Commands List", color=discord.Color.gold(), description='')
         e.set_author(name="Birthday Bot", icon_url="https://cdn.discordapp.com/avatars/767125663312117802/c1109ff318c462a0229cf814e9c85139.png?size=128")
-        e.set_footer(text="https://status.rawserver.tk/")
+        e.set_footer(text="No more status page lul.")
         for page in self.paginator.pages:
             e.description += page
         await destination.send(embed=e)
